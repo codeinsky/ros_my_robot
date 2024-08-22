38575d7e50377fc9a2b3373df485c27fb2175402
codeinsky <todorov@bezeqint.net>
2024-08-22 23:06:58 +0800

    project1 completed

--- src/project1/scripts/.ipynb_checkpoints/movement_service-checkpoint.py
+++ src/project1/scripts/.ipynb_checkpoints/movement_service-checkpoint.py
@@ -1,18 +1,63 @@
 import rospy 
 from std_srvs.srv import Empty, EmptyResponse, Trigger , TriggerResponse
+from std_msgs.msg import Bool, String
 from time import sleep 
+import sys 
+sys.path.insert(0,'../../Dofbot/0.py_install/Arm_Lib/')
+
+from Arm_Lib import Arm_Device
 
 is_busy = False 
-
+free_slot = "none"
 def movement_node():
     rospy.init_node('movement_serive_node', anonymous= True)
     service = rospy.Service("/start_movement", Trigger, start_movement)
+    empty_slot_sub = rospy.Subscriber('/storage_free_slot',String, update_free_slot)
     rospy.spin()
-
+    
+def update_free_slot(msg):
+    global free_slot
+    free_slot = msg.data
+    
 def start_movement(req):
-    rospy.loginfo("Movement sequence is starting")
-    sleep(5)
-    rospy.loginfo("Movement sequence competed")
+    global free_slot
+    sleep_time = 3
+    slots_coordinats = {
+        '8': [90, 81, 8, 28, 90],
+        '9': [129, 60, 24, 39, 93],
+        '10': [112, 65, 33, 17, 90]
+    }
+    print("Going to slot:",str(free_slot))
+    print("Slot coordinats of:"+ str(free_slot),":",slots_coordinats[str(free_slot)])
+    current_free_slot = slots_coordinats[str(free_slot)]
+    # movement sequince
+    Arm = Arm_Device()
+    Arm.Arm_Buzzer_On(1)
+    sleep(1)
+    Arm.Arm_Buzzer_On(0)
+    # go home 
+    Arm.Arm_serial_servo_write6(90,90,90,90,90,90,1500)
+    sleep(sleep_time)
+    # got to manual position 11 with open arm 
+    Arm.Arm_serial_servo_write6(55, 66, 21, 33, 89, 90, 2000)
+    sleep(sleep_time)
+    # arm close 
+    Arm.Arm_serial_servo_write(6,146,500)
+    sleep(sleep_time)
+    # go init with closed arm 
+    Arm.Arm_serial_servo_write6(88, 131, 1, 49, 90, 146 ,2000)
+    sleep(sleep_time)
+    #go to posiiton 8 
+    Arm.Arm_serial_servo_write6( *current_free_slot, 146, 2000)
+    sleep(sleep_time)
+    # open gripper 
+    Arm.Arm_serial_servo_write(6,90,500)
+    sleep(sleep_time)
+    #go to init with kube
+    Arm.Arm_serial_servo_write6(88, 131, 1, 49, 90, 90 ,2000)
+    sleep(sleep_time)
+    Arm.Arm_serial_servo_write6(90,90,90,90,90,90,1500)
+    del Arm    
     return TriggerResponse(success= True, message = "movement completed")
 
 if __name__ == "__main__":
--- src/project1/scripts/controller_node.py
+++ src/project1/scripts/controller_node.py
@@ -3,42 +3,58 @@ from std_msgs.msg import Bool, String
 from std_srvs.srv import Empty,Trigger
 import datetime 
 counter = 0 
-threshold = 100
+threshold = 20
+free_slot = "none"
 def controller():
     global move_service 
     global sensor_sub
     print("Controller node started")
     rospy.init_node("movement_controller_node", anonymous=True)
     sensor_sub = rospy.Subscriber('/sensor11_topic', String, sensor_callback)
+    empty_slot_sub = rospy.Subscriber('/storage_free_slot',String, update_free_slot)
     move_service = rospy.ServiceProxy("/start_movement", Trigger)
     rospy.spin()
 
+def update_free_slot(msg):
+    global free_slot
+    free_slot = msg.data
 def sensor_callback(msg):
     global threshold
     global counter
     global sensor_sub
+    global free_slot
     current_state = msg.data
-    print(current_state)
+    #print(current_state)
+    # count 50 times object detected to trigger the movement 
     if current_state == "0":
         counter += 1 
         print(f"Detected object. Count {counter}")
     else:
         counter = 0 
-        print("No object")
+        # print("No object")
     if counter == threshold:
         print("Starting the movement")
+        print("There is free slot:", free_slot)
         print(datetime.datetime.now().time())
         print("***************************")
         counter = 0 
-        # stop reading data from senor 
-        sensor_sub.unregister()
-        print("Stop reading data from the sensor during run")
-        # trigger the service to start the movement 
-        response = move_service()
-        if response.success: 
-            print(response.message)
-            print("reading sensor data")
-            sensor_sub = rospy.Subscriber('/sensor11_topic', String, sensor_callback)
+        
+        # check if there is empty slot to recive the object 
+        if free_slot != "none":
+            # stop reading data from senor 
+            sensor_sub.unregister()
+            print("Stop reading data from the sensor during run")
+            # trigger the service to start the movement 
+            response = move_service()
+            if response.success: 
+                print(response.message)
+                print("reading sensor data")
+                sensor_sub = rospy.Subscriber('/sensor11_topic', String, sensor_callback)
+            else: 
+                print("Movement failed")
+        else:
+            print("Sorry the is no empty slots to receive the item")
+                
             
             
 if __name__ == "__main__":
